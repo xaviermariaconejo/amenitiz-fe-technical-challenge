252ff4e87fff45b006ac8dfe41313df05d360157
Xavier Conejo <xavier.maria.conejo@gmail.com>
2025-05-08 18:41:59 +0200

    Add container class

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,7 +7,7 @@ interface HeaderProps {
 export const Header: FC<HeaderProps> = ({ title = "WikiChess" }) => {
   return (
     <header className="bg-green-600 text-white py-4 px-6 shadow-md">
-      <h1 className="text-3xl font-extrabold">{title}</h1>
+      <h1 className="container text-3xl font-extrabold">{title}</h1>
     </header>
   );
 };
